ede15519688be257ca2f82549cd6c2bcd4ae5198
钟启盛 <nekobible@outlook.com>
2019-07-08 14:02:05 +0800

    springBoot Demo upload

---
+++ src/main/java/hello/demo/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package hello.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created
+ */
+
+//@Controller
+//public class GreetingController {
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+//
+//}
+
