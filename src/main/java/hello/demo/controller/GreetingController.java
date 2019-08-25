b24c887cef57263aa83f0817b53d1df1d63ffa34
钟启盛 <nekobible@outlook.com>
2019-08-25 15:54:55 +0800

    add a test page.

--- src/main/java/hello/demo/controller/GreetingController.java
+++ src/main/java/hello/demo/controller/GreetingController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
  * Created
  */
 
-//@Controller
-//public class GreetingController {
-//    @GetMapping("/greeting")
-//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
-//
-//}
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
 
