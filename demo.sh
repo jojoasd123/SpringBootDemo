205f9d75ed9a5ebc88438b95c19013e2e35868db
钟启盛 <nekobible@outlook.com>
2019-08-29 16:48:30 +0800

    modify deploy script.

--- demo.sh
+++ demo.sh
@@ -2,18 +2,19 @@ cd ~/App/SpringBootDemo/
 git fetch --all
 git reset --hard origin/master
 git pull
+mvn clean compile flyway:migrate -Pproduction
 mvn clean compile -DskipTests package
-pid=$(ps aux | grep "demo" | grep -v grep | awk'{print $2}')
+pid=$(ps aux | grep "8080" | grep -v grep | awk '{print $2}')
 echo "kill -9 $pid"
-kill -9 $pid
+kill -9 "$pid"
 STATUS=$?
 if [ $STATUS -eq 0 ]
 then
-    echo "Kill previous process successfully."
-    echo "Begin restart community"
-    java -jar target/demo-0.0.1-SNAPSHOT.jar &
-    disown
-    echo "Community restart successfully."
+	echo "Kill previous process successfully."
+	echo "Begin restart demo"
+	java -jar target/demo-0.0.1-SNAPSHOT.jar &
+	disown
+	echo "Community restart successfully."
 else
-    echo "Kill previous process failed."
+	echo "Kill previous process failed."
 fi
