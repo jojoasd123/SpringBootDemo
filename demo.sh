34831b435801cab50970e8c6a56bfa30bc61d39a
钟启盛 <nekobible@outlook.com>
2019-08-29 12:28:30 +0800

    add deploy script.

---
+++ demo.sh
@@ -0,0 +1,17 @@
+cd ~/App/SpringBootDemo/
+git pull
+mvn clean compile -DskipTests package
+pid=$(ps aux | grep "community" | grep -v grep | awk'{print $2}')
+echo "kill -9 $pid"
+kill -9 $pid
+STATUS=$?
+if [ $STATUS -eq 0 ]
+then
+    echo "Kill previous process successfully."
+    echo "Begin restart community"
+    java -jar target/demo-0.0.1-SNAPSHOT.jar &
+    disown
+    echo "Community restart successfully."
+else
+    echo "Kill previous process failed."
+fi
